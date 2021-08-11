5b8722f90632976a86326b82e99f11f67a60c711
sayhello854123 <53084301+sayhello854123@users.noreply.github.com>
2021-08-12 00:50:29 +0800

    type 2 diabetes mellitus
    type 2 diabetes mellitus

---
+++ T2DM/011_Supplementary_hub_cor.R
@@ -0,0 +1,88 @@
+library(limma)
+library(ggplot2)
+library(ggpubr)
+library(ggExtra)
+library(patchwork)
+
+pFilter=0.05 
+gene="T cells CD4 memory resting" 
+immune=read.table('./00data/GSE20966/06.immune/3.txt',sep="\t",header=T,row.names=1,check.names=F)
+immune1 <- immune[c(1:10),]
+immune2 <- immune[c(11:20),]
+x=as.numeric(immune1[,1])
+y=as.numeric(immune1[,2])
+if(sd(y)>0.001){
+  df1=as.data.frame(cbind(x,y))
+  corT=cor.test(x,y,method="spearman")
+  cor=corT$estimate
+  pValue=corT$p.value}
+p1=ggplot(df1, aes(x, y)) + ggtitle("STY1") +
+  theme(plot.title = element_text(size=28,  family="serif",face = "bold",hjust = 0.5)) +
+  ylab(colnames(immune1)[2])+xlab(gene)+
+  geom_point()+ geom_smooth(method="lm",formula=y~x) + theme_bw()+
+  stat_cor(method = 'spearman', aes(x =x, y =y))
+
+y=as.numeric(immune1[,3])
+if(sd(y)>0.001){
+  df1=as.data.frame(cbind(x,y))
+  corT=cor.test(x,y,method="spearman")
+  cor=corT$estimate
+  pValue=corT$p.value}
+p2=ggplot(df1, aes(x, y)) + ggtitle("SPP1") +
+  theme(plot.title = element_text(size=28,  family="serif",face = "bold",hjust = 0.5)) +
+  ylab(colnames(immune1)[3])+xlab(gene)+
+  geom_point()+ geom_smooth(method="lm",formula=y~x) + theme_bw()+
+  stat_cor(method = 'spearman', aes(x =x, y =y))
+###
+y=as.numeric(immune1[,4])
+if(sd(y)>0.001){
+  df1=as.data.frame(cbind(x,y))
+  corT=cor.test(x,y,method="spearman")
+  cor=corT$estimate
+  pValue=corT$p.value}
+p3=ggplot(df1, aes(x, y)) + ggtitle("CDK5") +
+  theme(plot.title = element_text(size=28,  family="serif",face = "bold",hjust = 0.5)) +
+  ylab(colnames(immune1)[4])+xlab(gene)+
+  geom_point()+ geom_smooth(method="lm",formula=y~x) + theme_bw()+
+  stat_cor(method = 'spearman', aes(x =x, y =y))
+
+x=as.numeric(immune2[,1])
+y=as.numeric(immune2[,2])
+if(sd(y)>0.001){
+  df1=as.data.frame(cbind(x,y))
+  corT=cor.test(x,y,method="spearman")
+  cor=corT$estimate
+  pValue=corT$p.value}
+p4=ggplot(df1, aes(x, y)) + ggtitle("STY1") +
+  theme(plot.title = element_text(size=28,  family="serif",face = "bold",hjust = 0.5)) +
+  ylab(colnames(immune2)[2])+xlab(gene)+
+  geom_point()+ geom_smooth(method="lm",formula=y~x) + theme_bw()+
+  stat_cor(method = 'spearman', aes(x =x, y =y))
+
+y=as.numeric(immune2[,3])
+if(sd(y)>0.001){
+  df1=as.data.frame(cbind(x,y))
+  corT=cor.test(x,y,method="spearman")
+  cor=corT$estimate
+  pValue=corT$p.value}
+p5=ggplot(df1, aes(x, y)) + ggtitle("SPP1") +
+  theme(plot.title = element_text(size=28,  family="serif",face = "bold",hjust = 0.5)) +
+  ylab(colnames(immune2)[3])+xlab(gene)+
+  geom_point()+ geom_smooth(method="lm",formula=y~x) + theme_bw()+
+  stat_cor(method = 'spearman', aes(x =x, y =y))
+###
+y=as.numeric(immune2[,4])
+if(sd(y)>0.001){
+  df1=as.data.frame(cbind(x,y))
+  corT=cor.test(x,y,method="spearman")
+  cor=corT$estimate
+  pValue=corT$p.value}
+p6=ggplot(df1, aes(x, y)) + ggtitle("CDK5") +
+  theme(plot.title = element_text(size=28,  family="serif",face = "bold",hjust = 0.5)) +
+  ylab(colnames(immune2)[4])+xlab(gene)+
+  geom_point()+ geom_smooth(method="lm",formula=y~x) + theme_bw()+
+  stat_cor(method = 'spearman', aes(x =x, y =y))
+
+P <- (p1|p2|p3)/(p4|p5|p6)
+ggplot2::ggsave(filename = './00data/GSE20966/06.immune/COR.pdf',plot = P, width =8, height =6) 
+
