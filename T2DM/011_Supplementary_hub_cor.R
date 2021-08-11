fc3f7693eab743e3079ee9971c739c1519068929
sayhello854123 <53084301+sayhello854123@users.noreply.github.com>
2021-08-12 00:57:05 +0800

    Update 011_Supplementary_hub_cor.R

--- T2DM/011_Supplementary_hub_cor.R
+++ T2DM/011_Supplementary_hub_cor.R
@@ -6,9 +6,8 @@ library(patchwork)
 
 pFilter=0.05 
 gene="T cells CD4 memory resting" 
-immune=read.table('./00data/GSE20966/06.immune/3.txt',sep="\t",header=T,row.names=1,check.names=F)
-immune1 <- immune[c(1:10),]
-immune2 <- immune[c(11:20),]
+
+load('./01.data/09.immune_cor_input.RData') 
 x=as.numeric(immune1[,1])
 y=as.numeric(immune1[,2])
 if(sd(y)>0.001){
@@ -84,5 +83,5 @@ p6=ggplot(df1, aes(x, y)) + ggtitle("CDK5") +
   stat_cor(method = 'spearman', aes(x =x, y =y))
 
 P <- (p1|p2|p3)/(p4|p5|p6)
-ggplot2::ggsave(filename = './00data/GSE20966/06.immune/COR.pdf',plot = P, width =8, height =6) 
+ggplot2::ggsave(filename = './13.immunCell_hub_cor/cor.pdf',plot = P, width =8, height =6) 
 
